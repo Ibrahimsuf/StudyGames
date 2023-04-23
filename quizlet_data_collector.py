a05c26538480b94c23db0cc66b4b35e184c1b5a6
Ibrahim Sufi <isufi@macalester.edu>
2023-04-23 07:12:26 -0500

    Changed quizlet_data_collector to throw exception when there is Invalid Quizlet URL

--- quizlet_data_collector.py
+++ quizlet_data_collector.py
@@ -6,18 +6,20 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 }
 def get_data_from_quizlet(url):
-    id = url.split("/")[3]
-    api_url = "https://quizlet.com/webapi/3.4/studiable-item-documents?filters%5BstudiableContainerId%5D={}&filters%5BstudiableContainerType%5D=1&perPage=1000&page=1".format(id)
-    itemsResponse = requests.get(api_url, verify=False, headers=headers)
-    
-    rawJson = {"studiableDocumentData": json.loads(
-                    itemsResponse.text)["responses"][0]["models"]}
-    terms = []
-    definitions = []
+    id = url.split("quizlet.com/")[1].split("/")[0]
 
-    for flashcard in rawJson["studiableDocumentData"]["studiableItem"]:
-        terms.append(flashcard["cardSides"][0]["media"][0]["plainText"])
-        definitions.append(flashcard["cardSides"][1]["media"][0]["plainText"])
-    return terms, definitions
+    try:
+        api_url = "https://quizlet.com/webapi/3.4/studiable-item-documents?filters%5BstudiableContainerId%5D={}&filters%5BstudiableContainerType%5D=1&perPage=1000&page=1".format(id)
+        itemsResponse = requests.get(api_url, verify=False, headers=headers)
+        
+        rawJson = {"studiableDocumentData": json.loads(
+                        itemsResponse.text)["responses"][0]["models"]}
+        terms = []
+        definitions = []
 
-url = "https://quizlet.com/17994005/j20-background-of-kanji-flash-cards/?funnelUUID=15944f14-69a3-422f-8652-0282b485c804"
+        for flashcard in rawJson["studiableDocumentData"]["studiableItem"]:
+            terms.append(flashcard["cardSides"][0]["media"][0]["plainText"])
+            definitions.append(flashcard["cardSides"][1]["media"][0]["plainText"])
+        return terms, definitions
+    except:
+        raise Exception("Invalid Quzilet URL")
